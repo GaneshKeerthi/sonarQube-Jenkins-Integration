3b3ceeab145dd09f9cc4b25ba924d63014e6da80
Ganesh Keerthi <sganeshkeerthi@gmail.com>
2025-02-24 09:51:03 +0530

    First commit of the maven project

---
+++ src/main/java/com/tenseconds/MainApplication.java
@@ -0,0 +1,8 @@
+package com.tenseconds;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello SonarQube-Jenkins Integration");
+	}
+}
---
+++ src/test/resources/com/tenseconds/MainApplicationTest.java
@@ -0,0 +1,12 @@
+package com.tenseconds;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainApplicationTest {
+	@Test
+	void test() {
+		assertEquals(true, true);
+	}
+}
